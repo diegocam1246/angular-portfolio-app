f6298991a4617eba0170842f97ef7e4ff22f7e84
Diego <diegocam1246@gmail.com>
2024-12-13 10:25:16 -0500

    init

---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { CardComponent } from '../card/card.component';
+import { Card } from '../_models/card';
+import { Tag } from '../_models/tags';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.css',
+  providers: []
+})
+export class PortfolioComponent {
+
+  projects: Card[] = [
+  ]
+
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('Portfolio')
+  }
+
+  trackById(index: number, project: Card): number {
+    return project.id;
+  }
+
+}
